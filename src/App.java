37e29aedf259d4413c7460ded709f98a34e960fc
Bernardo Zamin <126703680+Bernardo-Zamin@users.noreply.github.com>
2023-08-09 18:06:07 -0300

    Ex2

--- src/App.java
+++ src/App.java
@@ -3,6 +3,8 @@ public class App {
 
         Carro basico = new Carro("Basico", TipoCombustivel.GASOLINA, 10, 55);
         Carro esportivo = new Carro("Esportivo", TipoCombustivel.GASOLINA, 10, 55);
+        Carro utilitario = new Carro("utilitario", TipoCombustivel.DIESEL, 5,70);
+
 
         System.out.println("Tipos de veiculos:");
         System.out.println(basico);
